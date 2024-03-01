b267603536f1353189c67d97ee2a936a53552a7e
kilian-gross <161361008+kilian-gross@users.noreply.github.com>
2024-03-01 22:13:33 +0100

    Moved css import to header.html

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/globals.css">
     <title>Home</title>
 </head>
+
 <body>
     <?php include "components/header.html" ?>
     <div class="content">
 
     </div>
 </body>
+
 </html>
