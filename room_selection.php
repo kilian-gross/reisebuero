8fe80b0339bda1979d40a9e123f02952177b8b74
Andrin-Halbeisen <161647757+Globus173@users.noreply.github.com>
2024-03-07 09:56:50 +0100

    implemented first form, troubles with image

--- room_selection.php
+++ room_selection.php
@@ -8,7 +8,14 @@
 <body>
     <?php # include "components/header.html";?>
     <?php $hotelname = "great hotel" ;?>
-    <?php echo "<h2>" . "Book now " . $hotelname . "<h2\>" ?>
+    <?php echo "<h2>" . "Book now " . $hotelname . "<h2\>" ?> <br>
+    <?php echo "<img src='htdocs/reisebuero/resources/images.jpeg'/>" ?> <br>
+    <?php echo "<p>" . "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam" . "<p>"?>
+
+    <form action="number_of:rooms.php" method="GET">
+    Anzahl Einzelzimmer: <input type="int" name="number_singlerooms"><br>
+    Anzahl Doppelzimmer: <input type="int" name="number_doublerooms"><br>
+    <input type="submit">
 
 </body>
 </html>
