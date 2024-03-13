76d6049a327cf70c77464aef36eb8db3a03d2b4d
kilian-gross <161361008+kilian-gross@users.noreply.github.com>
2024-03-13 16:34:35 +0100

    Form validation: require each field

--- personal_information.php
+++ personal_information.php
@@ -8,28 +8,28 @@
 <body>
     <form action="<?php __FILE__ ?>" method="post">
         <label for="name">Name</label>
-        <input type="text" id="name" >
+        <input type="text" id="name" required>
 
         <label for="surname">Surname</label>
-        <input type="text" id="surname" >
+        <input type="text" id="surname" required>
 
         <label for="email">Email</label>
-        <input type="email" id="email" >
+        <input type="email" id="email" required>
 
         <label for="date-of-birth">Date of birth</label>
-        <input type="date" id="date-of-birth" >
+        <input type="date" id="date-of-birth" required>
 
         <label for="phone-number">Phone number</label>
-        <input type="tel" id="phone-number" >
+        <input type="tel" id="phone-number" required>
 
         <label for="address">Address</label>
-        <input type="text" id="address" >
+        <input type="text" id="address" required>
 
         <label for="postal-code">Postal code</label>
-        <input type="text" id="postal-code" >
+        <input type="text" id="postal-code" required>
 
         <label for="city">City</label>
-        <input type="text" id="city" >
+        <input type="text" id="city" required>
 
         <input type="submit" value="Book">
     </form>
