5cb2406ede8a614af6888bff3e5f7124ab5baf36
kilian-gross <161361008+kilian-gross@users.noreply.github.com>
2024-03-13 16:35:20 +0100

    Moved inputs around

--- personal_information.php
+++ personal_information.php
@@ -13,11 +13,11 @@
         <label for="surname">Surname</label>
         <input type="text" id="surname" required>
 
-        <label for="email">Email</label>
-        <input type="email" id="email" required>
-
         <label for="date-of-birth">Date of birth</label>
         <input type="date" id="date-of-birth" required>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" required>
 
         <label for="phone-number">Phone number</label>
         <input type="tel" id="phone-number" required>
@@ -25,11 +25,11 @@
         <label for="address">Address</label>
         <input type="text" id="address" required>
 
-        <label for="postal-code">Postal code</label>
-        <input type="text" id="postal-code" required>
-
         <label for="city">City</label>
         <input type="text" id="city" required>
+
+        <label for="postal-code">Postal code</label>
+        <input type="text" id="postal-code" required>
 
         <input type="submit" value="Book">
     </form>
