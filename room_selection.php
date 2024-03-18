a1b79788af785923460f7488ee4453a10ec247f9
kilian-gross <61361008+kilian-gross@users.noreply.github.com>
2024-03-18 19:34:42 +0100

    Fixed some things

--- room_selection.php
+++ room_selection.php
@@ -10,22 +10,16 @@
     <?php $hotelname = "great hotel" ; ?>
     <?php echo "Book now"  ; $hotelname ; #"<h2\>"; ?> <br>
     <?php #echo "<img src='htdocs/reisebuero/resources/images.jpeg'/>" ?> <br>
-    <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam <p>
 
     <form action="room_selection.php" method="POST">
-    number of singlerooms: <input type="number" name="number_singlerooms" required><br>
-    number of doublerooms: <input type="number" name="number_doublerooms" required><br>
-    number of suites: <input type="number" name="number_suites" rqu><br>
+    Number of singlerooms: <input type="number" name="number_singlerooms" required><br>
+    Number of doublerooms: <input type="number" name="number_doublerooms" required><br>
+    Number of suites: <input type="number" name="number_suites" required><br>
     <input type="submit">
 
     <?php
     if (isset($_POST['number_suites'])) {
         $Total = $_POST["number_singlerooms"]*200 + $_POST["number_doublerooms"]*320 + $_POST["number_suites"]*400; 
-        echo "Price: <br>";
-        echo "price single rooms:" . $_POST["number_singlerooms"]*200 . "CHF <br>";
-        echo "price double rooms:" . $_POST["number_doublerooms"]*320 . "CHF <br>";
-        echo "price suites" . $_POST["number_suites"]*400 . "CHF <br>";
-        echo "total:". $Total . "CHF";
         setcookie("amount_suite", $_POST["number_suites"]);
         setcookie("amount_single_room", $_POST["number_singlerooms"]);
         setcookie("amount_double_room", $_POST["number_doublerooms"]);
@@ -33,10 +27,5 @@
         
     }
     ?>
-    <script>
-        if (document.cookie != "") {
-            window.location.href = "personal_information.php";
-        }
-    </script>
 </body>
 </html>
