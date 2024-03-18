d2a9596dea0cd7171eba38f396d6133291c61246
GurkusTheHoly <161693468+GurkusTheHoly@users.github.com>
2024-03-18 20:29:56 +0100

    Added the about us section with some text

---
+++ about-us.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/globals.css">
+    <title>about-us</title>
+</head>
+
+<body>
+    overflow-x: hidden;
+
+    <?php include "components/header.html" ?>
+    <body>
+
+        <?php include "components/about-us.html"?>
+    </body>
+    <?php include "components/footer.html" ?>
+
+
+    
+</body>
+
+</html>
