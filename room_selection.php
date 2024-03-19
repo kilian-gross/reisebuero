306cc42448eab4fa8d41d4942f34d311d754474b
kilian-gross <61361008+kilian-gross@users.noreply.github.com>
2024-03-19 16:43:48 +0100

    No negtive room values

--- room_selection.php
+++ room_selection.php
@@ -10,9 +10,9 @@
     <?php include "components/header.html";?>
     <div class="form-container">
         <form action="personal_information.php" method="POST">
-        Number of singlerooms: <input type="number" name="amount_single_room" required><br>
-        Number of doublerooms: <input type="number" name="amount_double_room" required><br>
-        Number of suites: <input type="number" name="amount_suite" required><br>
+        Number of singlerooms: <input type="number" name="amount_single_room" required min="0"><br>
+        Number of doublerooms: <input type="number" name="amount_double_room" required min="0"><br>
+        Number of suites: <input type="number" name="amount_suite" required min="0"><br>
         <input type="submit" id="submit-button" value="Book">
 
     </div>
